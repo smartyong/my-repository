7fd94e509caf180adf1f71b8fe8adae570dde65f
smartyong <315274305@qq.com>
2020-05-26 07:30:22 +0800

    GroupChat update

--- src/main/java/com/example/study/demo/netty/nio/GroupChatClient.java
+++ src/main/java/com/example/study/demo/netty/nio/GroupChatClient.java
@@ -1,20 +1,94 @@
 package com.example.study.demo.netty.nio;
 
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 /**
+ * 发送消息，接收消息
  * Created by s94pcp on 2020/5/25.
  */
 public class GroupChatClient {
     private Selector selector;
+    private SocketChannel socketChannel;
     private static final String HOST = "127.0.0.1";
     private static final int PORT = 8888;
+    private String name;
 
-    public GroupChatClient() {
-
-
-
+    public GroupChatClient() throws Exception {
+        selector = Selector.open();
+        socketChannel = SocketChannel.open(new InetSocketAddress(HOST, PORT));
+        socketChannel.configureBlocking(false);
+        //注册到 selector
+        socketChannel.register(selector, SelectionKey.OP_READ);
+        name = socketChannel.getLocalAddress().toString().substring(1);
     }
 
+    //发送消息
+    public void sendMsg(String msg) throws Exception {
+        msg = name + "说:" + msg;
+        socketChannel.write(ByteBuffer.wrap(msg.getBytes()));
+    }
+
+    //读取消息
+    public void readMsg(){
+        try {
+            int count = selector.select(1000);
+            if (count > 0) {
+                System.out.println("count is:" + count);
+                Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
+                while (keyIterator.hasNext()) {
+                    SelectionKey key = keyIterator.next();
+                    if (key.isReadable()) {
+                        System.out.println("开始读取信息........");
+                        SocketChannel channel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        //读取到buffer
+                        channel.read(buffer);
+                        //打印
+                        System.out.println(new String(buffer.array()).trim());
+                    }
+                    keyIterator.remove();
+                }
+            } else {
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        GroupChatClient groupChatClient = new GroupChatClient();
+        new Thread(
+                () -> {
+                    //读取数据
+                    while (true) {
+                        groupChatClient.readMsg();
+                        try {
+                            TimeUnit.SECONDS.sleep(3);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).start();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String msg = scanner.nextLine();
+            groupChatClient.sendMsg(msg);
+
+        }
+
+    }
 
 }
--- src/main/java/com/example/study/demo/netty/nio/GroupChatServer.java
+++ src/main/java/com/example/study/demo/netty/nio/GroupChatServer.java
@@ -1,9 +1,11 @@
 package com.example.study.demo.netty.nio;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.Iterator;
+import java.util.Set;
 
 /**
  * Created by s94pcp on 2020/5/25.
@@ -13,6 +15,7 @@ public class GroupChatServer {
     private Selector selector;
     private ServerSocketChannel ssc;
     private static final int PORT = 8888;
+
 
     public GroupChatServer() throws Exception {
         selector = Selector.open();
@@ -30,7 +33,7 @@ public class GroupChatServer {
         while (true) {
             int count = selector.select(1000);
             if (count>0){
-                System.out.println("监听到客户端事件....");
+//                System.out.println("监听到客户端事件....");
                 Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
                 while (keyIterator.hasNext()){
                     SelectionKey key = keyIterator.next();
@@ -43,9 +46,9 @@ public class GroupChatServer {
                         System.out.println(channel.getRemoteAddress().toString().substring(1)+"已上线..");
                     }
                     if (key.isReadable()){
-                        //往其他客户端推送消息
-                        SocketChannel channel = (SocketChannel)key.channel();
-                        sendMsgToOthers(channel);
+                        //读取消息
+                        readData(key);
+
                     }
                     //remove
                     keyIterator.remove();
@@ -59,19 +62,43 @@ public class GroupChatServer {
 
     }
 
-    public  void  sendMsgToOthers(SocketChannel selfChannel) throws Exception{
-        Iterator<SelectionKey> keyIterator = selector.selectedKeys().iterator();
-        while (keyIterator.hasNext()){
-            SelectionKey next = keyIterator.next();
-            SocketChannel channel = (SocketChannel)next.channel();
-            if (channel!=selfChannel){
-                //
-                ByteBuffer buffer=ByteBuffer.allocate(1024);
-                selfChannel.read(buffer);
-                buffer.flip();
-                channel.write(buffer);
+    public  void readData(SelectionKey key)throws  Exception{
+        try{
+        SocketChannel channel = (SocketChannel)key.channel();
+        ByteBuffer buffer=ByteBuffer.allocate(1024);
+        channel.read(buffer);
+        String msg=new String(buffer.array());
+        System.out.println("来自客户端消息："+msg);
+        //往其他客户端推送消息
+        sendMsgToOthers(msg,channel);
+        }catch (Exception e){
+            e.printStackTrace();
+            SocketChannel channel = (SocketChannel)key.channel();
+            try {
+                System.out.println(channel.getRemoteAddress().toString().substring(1)+"已下线..");
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
+            //取消注册
+            key.cancel();
+            channel.close();
+        }
+    }
 
+    public  void  sendMsgToOthers(String msg,SocketChannel selfChannel) throws Exception{
+        //selector.keys()  获取已经注册到selector 的所有 channel
+        System.out.println("服务器转发消息中......");
+        Set<SelectionKey> keys =  selector.keys();
+        for (SelectionKey key:keys) {
+            Channel channel = key.channel();//不能写成 SocketChannel dest=(SocketChannel)key.channel();
+            if (channel instanceof SocketChannel &&channel!=selfChannel){
+                //
+                SocketChannel dest=(SocketChannel)channel;
+//                ByteBuffer buffer=ByteBuffer.allocate(1024);
+//                buffer.put(msg.getBytes());
+                dest.write(ByteBuffer.wrap(msg.getBytes()));
+                System.out.println("转发至："+dest.getRemoteAddress().toString().substring(1)+"完成！");
+            }
         }
 
     }
